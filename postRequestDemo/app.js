acc9ec0d426ef7ca7de5c2467327edbd0c6c22c4
Rain <rainrivas@gmail.com>
2017-02-24 20:48:58 -0700

    Adding body parser package and modifying routes to now handle reading data from the form

--- postRequestDemo/app.js
+++ postRequestDemo/app.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+
+var friends = ["Josh", "Duc", "Lily", "John", "Jane"];
 
 app.get("/", function(req, res) {
     res.render("home");
 });
 
-app.post("/addfriend",function(req, res){
-	res.send(req.params.friend);
+app.post("/addfriend", function(req, res) {
+	friends.push(req.body.friend);
+    res.redirect('/friends');
 });
 
-app.get("/friends",function(req,res){
-	var friends = ["Josh","Duc","Lily","John","Jane"];
-	res.render("friends",{friends: friends}); // ejsVarName: localFileVarName
+app.get("/friends", function(req, res) {
+
+    res.render("friends", { friends: friends }); // ejsVarName: localFileVarName
 });
 
 app.listen(3000, function() {
