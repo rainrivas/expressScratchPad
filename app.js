da8ac5729a8f79b3b3fb4a04d30ec2cb72ce27ef
Rain <rainrivas@gmail.com>
2017-02-26 22:43:09 -0700

    Adding gomix README. Also created EJS views a folder to house them. Modified App.js to now render ejs files and added routes for posting search.

--- app.js
+++ app.js
@@ -2,29 +2,33 @@ var express = require('express');
 var app = express();
 var request = require('request');
 
-// app.use(express.static("public")); // tell express to serve the contents of public directory
-app.set("view engine","ejs");
+app.use(express.static("public")); // tell express to serve the contents of public directory
+app.set("view engine", "ejs");
 
-app.get('/',function(req,res){
-	res.send("Welcome to move search. Build in Progress");
+app.get('/', function(req, res) {
+    res.render("home");
 });
 
-app.get('/results',function(req,res){
-	request("http://www.omdbapi.com/?s=star",function(error,response,body){
-		if(!error && response.statusCode === 200){
-			var movies = JSON.parse(body);
-			console.log(movies);
-			res.send(movies);
-			}
-			else{
-				console.log(error);
-				res.send("We hit an error")
-			}
-	});
-	// res.send("Good we working");
+app.get('/results', function(req, res) {
+    request("http://www.omdbapi.com/?s=star", function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var movies = JSON.parse(body);
+            console.log(movies);
+            res.send(movies);
+        } else {
+            console.log(error);
+            res.send("We hit an error")
+        }
+    });
+    // res.send("Good we working");
 });
-app.listen(3000,function(){
-	console.log("App running on port 3000");
+
+app.post("/search", function(req, res) {
+    res.redirect('/results');
+});
+
+app.listen(3000, function() {
+    console.log("App running on port 3000");
 });
 
 // for GoMix use below
