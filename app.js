13b58d4baf9e5c2b47c74ac96e1b8980d8aea3c2
Rain <rainrivas@gmail.com>
2017-02-26 21:57:30 -0700

    Setting up the basic skeleton for movie search

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+
+// app.use(express.static("public")); // tell express to serve the contents of public directory
+app.set("view engine","ejs");
+
+app.get('/',function(req,res){
+	res.send("Welcome to move search. Build in Progress");
+});
+
+app.get('/results',function(req,res){
+	request("http://www.omdbapi.com/?s=star",function(error,response,body){
+		if(!error && response.statusCode === 200){
+			var movies = JSON.parse(body);
+			console.log(movies);
+			res.send(movies);
+			}
+			else{
+				console.log(error);
+				res.send("We hit an error")
+			}
+	});
+	// res.send("Good we working");
+});
+app.listen(3000,function(){
+	console.log("App running on port 3000");
+});
+
+// for GoMix use below
+/*
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+*/
